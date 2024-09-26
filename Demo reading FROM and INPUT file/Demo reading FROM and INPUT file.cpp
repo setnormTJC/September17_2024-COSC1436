0e927d154a09b56648ff1e0b3793a611e967c992
Work <Work@MSI>
2024-09-26 09:19:58 -0500

    Mid-class on September 26 - finished Topic 3

--- Demo reading FROM and INPUT file/Demo reading FROM and INPUT file.cpp
+++ Demo reading FROM and INPUT file/Demo reading FROM and INPUT file.cpp
@@ -10,21 +10,28 @@
 int main()
 {
 	//std::ifstream fin("someInputFile.txt"); //a "relative" filepath
-
-	std::ifstream fin
-	("C:\\Users\\Work\\source\\repos\\September17_2024 - COSC1436\\Demo reading FROM and INPUT file\\someInputFile.txt");
+	
+//C:\Users\snorm\source\repos\WillItWork
+	std::ifstream fin("C://Users/Work/Downloads/someInputFile.txt");
+	//("C:\\Users\\Work\\source\\repos\\September17_2024 - COSC1436\\Demo reading FROM and INPUT file\\someInputFile.txt");
 	//"absolute" filepath 
 
-	if (!fin.is_open())
+
+	if (!fin.is_open()) //we have not discussed "if" statements/"conditionals" yet, but we will later today 
 	{
-		std::cout << "FNFE\n";
+		std::cout << "FNFE\n"; //"file not found error" 
 		return 1; 
 	}
 
 	std::string lineFromFile; 
-
+	std::string secondLineFromFile; 
 	//std::getline(std::cin, lineFromFile); 
 	std::getline(fin, lineFromFile);
+	std::getline(fin, secondLineFromFile);
 
-	std::cout << "File contents are: " << lineFromFile << "\n";
+	fin.close(); 
+
+	std::cout << "File contents are: " << "\n";
+	std::cout << lineFromFile << "\n";
+	std::cout << secondLineFromFile << "\n";
 }
